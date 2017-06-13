34d8e1c552cf250a9c86d491df09189a7d41c934
test <>
2017-06-13 11:53:29 +0300

    fix seed error

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 Setting.delete_all
 s = Setting.new
 s.preferences = {"index_v":"1","country_v":"0","country_code_v":"0","threat_id_v":"0","host_v":"0","threat_tri_v":"0","risk_v":"0","asn_v":"0","asn_registry_v":"0",
-,"confidence_v":"0","continent_code_v":"0","location_v":"0","logid_v":"0",
-,"message":"0","source_ids_v":"0","threat_type_v":"0","type_v":"0",
-,"type_description_v":"0","global_filter_time_v":"0","lgstash_backend_v":"0","logstash_febe_latency_sec_v":"0",
+"confidence_v":"0","continent_code_v":"0","location_v":"0","logid_v":"0",
+"message":"0","source_ids_v":"0","threat_type_v":"0","type_v":"0",
+"type_description_v":"0","global_filter_time_v":"0","lgstash_backend_v":"0","logstash_febe_latency_sec_v":"0",
 "logstash_frontend_v":"0","vendor_filter_time_v":"0","id_v":"0","srcevent_v":"0","timestamp_v":"0","version_v":"0"}
 s.save
