c28808724c42df7b28f84ca9cae650ec657e58e2
test <>
2017-06-13 21:47:57 +0300

    refactoring

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,10 @@ class SearchController < ApplicationController
   require 'elasticsearch/persistence/model'
   require 'elasticsearch/dsl'
 
-   def settings
+  def settings
 
 
-   end
+  end
 
   def index
     #Search.connection
@@ -22,42 +22,42 @@ class SearchController < ApplicationController
   end
 
   def edit
-   @Search = Search.search(query:{match: {_id: params[:id]}}).first
+    @Search = Search.search(query:{match: {_id: params[:id]}}).first
   end
 
 
- def update
+  def update
     @s = Search.search(query:{match: {_id: params[:id]}}).first
     @s.update_attributes(
-      threat_id: params[:threat_id],
-      asn_registry: params[:asn_registry],
-      threat_tri: params[:threat_tri].to_f,
-      risk: params[:risk].to_f,
-      country:  params[:country],
-      asn:  params[:asn],
+    threat_id: params[:threat_id],
+    asn_registry: params[:asn_registry],
+    threat_tri: params[:threat_tri].to_f,
+    risk: params[:risk].to_f,
+    country:  params[:country],
+    asn:  params[:asn],
 
 
-          confidence:  params[:confidence],
-            continent_code:  params[:continent_code],
-              location:  params[:location],
-                logid:  params[:logid],
-                  message:  params[:message],
-                    source_ids:  params[:source_ids],
-                    threat_type:  params[:threat_type],
-                      type:  params[:continent_code],
-                        type_description:  params[:type_description],
-                          
-                            global_filter_time:  params[:global_filter_time],
-                              logstash_backend:  params[:logstash_backend],
-                              logstash_febe_latency_sec:  params[:logstash_febe_latency_sec],
-                                logstash_frontend:  params[:logstash_frontend],
-                                raw_message_bytesize:  params[:raw_message_bytesize],
-                                  vendor_filter_time:  params[:vendor_filter_time],
-                                    id:  params[:id],
-                                      srcevent:  params[:srcevent],
-                                        timestamp:  params[:timestamp],
-                                          vendor:  params[:vendor],
-                                            version:  params[:version]
+    confidence:  params[:confidence],
+    continent_code:  params[:continent_code],
+    location:  params[:location],
+    logid:  params[:logid],
+    message:  params[:message],
+    source_ids:  params[:source_ids],
+    threat_type:  params[:threat_type],
+    type:  params[:continent_code],
+    type_description:  params[:type_description],
+
+    global_filter_time:  params[:global_filter_time],
+    logstash_backend:  params[:logstash_backend],
+    logstash_febe_latency_sec:  params[:logstash_febe_latency_sec],
+    logstash_frontend:  params[:logstash_frontend],
+    raw_message_bytesize:  params[:raw_message_bytesize],
+    vendor_filter_time:  params[:vendor_filter_time],
+    id:  params[:id],
+    srcevent:  params[:srcevent],
+    timestamp:  params[:timestamp],
+    vendor:  params[:vendor],
+    version:  params[:version]
     )
     flash[:success] = 'Record successfully updated!'
     redirect_to search_index_path
