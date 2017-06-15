7e6b5ffdde567f52dda994c2945815cc7824860f
Nikos Maris <nmaris@atypon.com>
2017-06-15 17:52:54 +0300

    fix dc.js url for cdn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def cdn(lib, version, minified='', component='')
-    "https://cdnjs.cloudflare.com/ajax/libs/${lib}/${version}/${component}${minified}"
+  def cdn(lib, version, minified='', component=lib)
+    "https://cdnjs.cloudflare.com/ajax/libs/#{lib}/#{version}/#{component}#{minified}"
   end
   def dcjs
     cdn('dc', '2.0.2')
