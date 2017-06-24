ed9a3c9dc0028cf2ba41dda90eb10e54f93f22fc
frcake <mpdevcake@gmail.com>
2017-06-24 21:18:12 +0300

    Index changes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
- before_action :require_admin ,only: [:edit,:update,:destroy]
+  before_action :require_admin ,only: [:edit,:update,:destroy]
   require 'elasticsearch/persistence/model'
   require 'elasticsearch/dsl'
 
@@ -12,16 +12,16 @@ class SearchController < ApplicationController
     #Search.connection
     #@Search = Search.search(query: { match: {country_code: params[:q]}}) unless params[:q].nil?
     if !params[:q]
-      @Search = Search.all( size:50)# unless params[:q].nil?
+      @Search = Search.all(size:50)# unless params[:q].nil?
     else
-      @Search = Search.search(query: { match: {threat_id: params[:q]}},size:50) unless params[:q].nil?
+      @Search = Search.search(query: { match: {threat_id: params[:q]}},size:10) unless params[:q].nil?
     end
     #byebug
     @setting = Hashie::Mash.new Setting.first.preferences
     @single = Setting.first
   end
   def show
-   @Search  = Search.search(query:{match: {_id: params[:id]}}).first
+    @Search  = Search.search(query:{match: {_id: params[:id]}}).first
   end
 
   def edit
@@ -32,32 +32,32 @@ class SearchController < ApplicationController
   def update
     @s = Search.search(query:{match: {_id: params[:id]}}).first
     @s.update_attributes(
-    threat_id: params[:threat_id],
-    asn_registry: params[:asn_registry],
-    threat_tri: params[:threat_tri].to_f,
-    risk: params[:risk].to_f,
-    country:  params[:country],
-    asn:  params[:asn],
-    confidence:  params[:confidence],
-    continent_code:  params[:continent_code],
-    location:  params[:location].to_f,
-    logid:  params[:logid],
-    message:  params[:message],
-    source_ids:  params[:source_ids],
-    threat_type:  params[:threat_type],
-    type:  params[:continent_code],
-    type_description:  params[:type_description],
-    global_filter_time:  params[:global_filter_time],
-    logstash_backend:  params[:logstash_backend],
-    logstash_febe_latency_sec:  params[:logstash_febe_latency_sec],
-    logstash_frontend:  params[:logstash_frontend],
-    raw_message_bytesize:  params[:raw_message_bytesize],
-    vendor_filter_time:  params[:vendor_filter_time],
-    id:  params[:id],
-    srcevent:  params[:srcevent],
-    timestamp:  params[:timestamp],
-    vendor:  params[:vendor],
-    version:  params[:version]
+      threat_id: params[:threat_id],
+      asn_registry: params[:asn_registry],
+      threat_tri: params[:threat_tri].to_f,
+      risk: params[:risk].to_f,
+      country:  params[:country],
+      asn:  params[:asn],
+      confidence:  params[:confidence],
+      continent_code:  params[:continent_code],
+      location:  params[:location].to_f,
+      logid:  params[:logid],
+      message:  params[:message],
+      source_ids:  params[:source_ids],
+      threat_type:  params[:threat_type],
+      type:  params[:continent_code],
+      type_description:  params[:type_description],
+      global_filter_time:  params[:global_filter_time],
+      logstash_backend:  params[:logstash_backend],
+      logstash_febe_latency_sec:  params[:logstash_febe_latency_sec],
+      logstash_frontend:  params[:logstash_frontend],
+      raw_message_bytesize:  params[:raw_message_bytesize],
+      vendor_filter_time:  params[:vendor_filter_time],
+      id:  params[:id],
+      srcevent:  params[:srcevent],
+      timestamp:  params[:timestamp],
+      vendor:  params[:vendor],
+      version:  params[:version]
     )
     flash[:success] = 'Record successfully updated!'
     redirect_to search_index_path
@@ -77,15 +77,15 @@ class SearchController < ApplicationController
   private
 
   def require_admin
-   if current_user
-   unless current_user.is_admin
+    if current_user
+      unless current_user.is_admin
         flash[:error] = "You must be an administrator in to access this section"
         redirect_to root_path
-   end
-  else
-    flash[:error] = "You must be an logged in to access this section"
-    redirect_to root_path
-   end
+      end
+    else
+      flash[:error] = "You must be an logged in to access this section"
+      redirect_to root_path
+    end
   end
 
 end
