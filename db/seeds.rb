7e088a04dd65a776e30002b4bbdbfaa035f7f855
test <>
2017-07-02 12:08:20 +0300

    add host as default setting

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Setting.delete_all
 s = Setting.new
-s.preferences = {"index":"0","country":"1","country_code":"0","threat_id":"1","host":"0","threat_tri":"0","risk":"0","asn":"0","asn_registry":"0",
+s.preferences = {"index":"0","country":"1","country_code":"0","threat_id":"1","host":"1","threat_tri":"0","risk":"0","asn":"0","asn_registry":"0",
   "category":"0","category_description":"0","city":"0", "confidence":"0","continent_code":"0","location":"0","logid":"0",
   "message":"0","source_ids":"0","threat_type":"0","type":"0",
   "type_description":"0","global_filter_time":"0","lgstash_backend":"0","logstash_febe_latency_sec":"0",
